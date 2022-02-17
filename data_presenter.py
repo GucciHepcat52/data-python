1af66b5595df5acb1907b5df2913d58f1fdeb5d5
Dallin Breen <dallinbreen624@gmail.com>
2022-02-17 14:39:38 -0700

    Finished python lab and added graph

---
+++ data_presenter.py
@@ -0,0 +1,59 @@
+from re import L
+import matplotlib.pyplot as plt
+
+open_file = open('CupcakeInvoices.csv')
+
+# for row in open_file:
+#     print(row)
+
+# for row in open_file:
+#     values = row.split(',')
+#     print(values[2])
+
+# for row in open_file:
+#     values = row.split(',')
+#     quantity = float(values[3])
+#     price = float(values[4])
+#     total = quantity * price
+#     print(total)
+
+# invoice_total = 0
+
+# for row in open_file:
+#     values = row.split(',')
+    # quantity = float(values[3])
+    # price = float(values[4])
+    # total = quantity * price
+
+#     invoice_total += total
+
+# print(invoice_total)
+
+
+chocolate_total = 0
+vanilla_total = 0
+strawberry_total = 0
+
+for row in open_file:
+    values = row.split(',')
+    quantity = float(values[3])
+    price = float(values[4])
+    total = quantity * price
+
+    if values[2] == 'Chocolate':
+        chocolate_total += total
+    elif values[2] == 'Vanilla':
+        vanilla_total += total
+    else:
+        strawberry_total += total
+
+
+
+fig = plt.figure()
+ax = fig.add_axes([0, 0, 1, 1])
+flavors = ['Chocolate', 'Vanilla', 'Strawberry']
+totals = [chocolate_total, vanilla_total, strawberry_total]
+ax.bar(flavors, totals)
+plt.show()
+
+open_file.close()
